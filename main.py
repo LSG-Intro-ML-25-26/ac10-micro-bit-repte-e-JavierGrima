a6d7c42423a5478ed511050e6ecf7a9f4160115c
Javier Grima Vivas <javier.grima@gracia.lasalle.cat>
2025-12-03 20:49:16 +0100

    Temperatura

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from microbit import *
 
+temperatura_maxima = 50
+
+while True:
+    temperatura_actual = input.temperature()
+
+    led.plot_bar_graph(temperatura_actual, temperatura_maxima)
--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+let temperatura_actual: number;
+let temperatura_maxima = 50
+while (true) {
+    temperatura_actual = input.temperature()
+    led.plotBarGraph(temperatura_actual, temperatura_maxima)
+}
