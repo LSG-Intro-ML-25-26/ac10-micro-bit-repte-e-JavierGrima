e5889ad134a6f106fc47c3e2c99797846bb8ff18
Javier Grima Vivas <javier.grima@gracia.lasalle.cat>
2025-12-03 21:39:33 +0100

    moure gota

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from microbit import *
 
+
 def grafico_temperatura():
     temperatura_maxima = 50
-
+    
     while True:
         temperatura_actual = input.temperature()
-
         led.plot_bar_graph(temperatura_actual, temperatura_maxima)
+
+def moure_gota():
+    x = 2
+    y = 2
+
+    while True:
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+
+        if accX <= 150 and x > 0:
+            x = x - 1
+        if accX > 150 and x < 4:
+            x = x + 1
+
+        if accY <= 150 and y > 0:
+            y = y - 1
+        if accY > 150 and y < 4:
+            y = y + 1
+
+input.on_button_pressed(Button.A, grafico_temperatura)
+input.on_button_pressed(Button.B, moure_gota)
--- main.ts
+++ main.ts
@@ -1,9 +1,37 @@
-function grafico_temperatura() {
+input.onButtonPressed(Button.A, function grafico_temperatura() {
     let temperatura_actual: number;
     let temperatura_maxima = 50
     while (true) {
         temperatura_actual = input.temperature()
         led.plotBarGraph(temperatura_actual, temperatura_maxima)
     }
-}
-
+})
+input.onButtonPressed(Button.B, function moure_gota() {
+    let accX: number;
+    let accY: number;
+    let x = 2
+    let y = 2
+    while (true) {
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        if (accX <= 150 && x > 0) {
+            x = x - 1
+        }
+        
+        if (accX > 150 && x < 4) {
+            x = x + 1
+        }
+        
+        if (accY <= 150 && y > 0) {
+            y = y - 1
+        }
+        
+        if (accY > 150 && y < 4) {
+            y = y + 1
+        }
+        
+    }
+})
